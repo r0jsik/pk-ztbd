a9913ec3ec458a7b9395aecbf2a010a16bcd6052
morsisko <morsisko@gmail.com>
2023-05-13 23:01:28 +0200

    Added basic implementation of mongo repository

--- pk/repo/mongo.py
+++ pk/repo/mongo.py
@@ -1,14 +1,40 @@
 import pymongo
 from pk.repo.repository import Repository
+from pk.objects.song import Song
+from pk.objects.genre import Genre
+from pk.objects.author import Author
 
 class MongoRepository(Repository):
 	def __init__(self):
-		self.__connection = pymongo.MongoClient("mongodb://localhost:27017/", username="mongo", password="mongo")
+		self.__connection = pymongo.MongoClient("mongodb://localhost:27017/", username="root", password="mongo")
 		self.__database = self.__connection["mongo"]
-		self.__collection = self.__database["your-collection-name"]
+		self.__collection = self.__database["songs"]
+
+	def create(self):
+		self.__collection.drop()
 	
 	def insert(self, item):
-		self.__collection.insert_one(item)
+		to_insert = {
+			"_id" : item.id,
+			"title" : item.title,
+			"genre" : {
+				"_id" : item.genre.id,
+				"name" : item.genre.name
+			},
+			"artist" : {
+				"_id" : item.artist.id,
+				"name" : item.artist.name
+			},
+			"year" : item.year,
+			"views" : item.views,
+			"features" : item.features,
+			"lyrics" : item.lyrics,
+			"lang_cld3" : item.lang_cld3,
+			"lang_ft" : item.lang_ft,
+			"language" : item.language
+		}
+
+		self.__collection.insert_one(to_insert)
 	
 	def update(self, item):
 		pass
@@ -20,4 +46,16 @@ class MongoRepository(Repository):
 		results = self.__collection.find({})
 		
 		for result in results:
-			yield result
+			yield Song(
+				result["_id"],
+				result["title"],
+				Genre(result["genre"]["_id"], result["genre"]["name"]),
+				Author(result["artist"]["_id"], result["artist"]["name"]),
+				result["year"],
+				result["views"],
+				result["features"],
+				result["lyrics"],
+				result["lang_cld3"],
+				result["lang_ft"],
+				result["language"]
+			)
